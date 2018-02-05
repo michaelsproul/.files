3e99258dfcc7ca358aac14f81e43883c29b1a5e0
Michael Sproul <micsproul@gmail.com>
2018-02-05 21:17:26 +1100

    Clean up fish common config

--- config/fish/common.fish
+++ config/fish/common.fish
@@ -4,10 +4,13 @@ set -gx PATH ~/.local/bin ~/Applications $PATH
 # Vim
 set -gx EDITOR vim
 
+# Safe mv and cp.
+alias mv "mv -i"
+alias cp "cp -i"
+
 # Python
 set -gx PYTHONSTARTUP "$HOME/.pyrc.py"
 set -gx PYTHONDONTWRITEBYTECODE 1
 
 # Rust
 alias ru=rustup
-set -x CARGO_INCREMENTAL 1
