89e9e91965f1632b195c2a197f623789a545f58c
Michael Sproul <michael@sigmaprime.io>
2022-01-28 14:48:41 +1100

    Misc tweaks

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,3 +1,7 @@
 function fish_prompt
-    echo -e -n -s (color -o green $USER) ": " (color -o blue (prompt_pwd)) " > "
+    if [ -n "$SSH_CLIENT" -o -n "$SSH_TTY" ]
+        echo -e -n -s (color brgreen $USER) "@" (color yellow $hostname) ": " (color brblue (prompt_pwd)) " > "
+    else
+        echo -e -n -s (color -o green $USER) ": " (color -o blue (prompt_pwd)) " > "
+    end
 end
