de3d3d9611299b7b9ee5a6739bae32cfdc16d122
Michael Sproul <michael@sigmaprime.io>
2022-01-28 14:50:27 +1100

    Improved github-remote

--- config/fish/functions/github-remote.fish
+++ config/fish/functions/github-remote.fish
@@ -1,5 +1,5 @@
 function github-remote
     set user $argv[1]
-    set reponame (basename (git rev-parse --show-toplevel))
+    set reponame (basename (git config --get remote.origin.url) .git)
     git remote add $user "git@github.com:$user/$reponame.git"
 end
